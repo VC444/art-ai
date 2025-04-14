0901413a10db881299b43f3cab69b8ec984e70f6
Vignesh Chandrasekharan <vchan444@gmail.com>
2025-04-14 19:25:55 +0530

    remove console log

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -9,8 +9,6 @@ export async function GET(request: Request) {
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
 
-  console.log({ code });
-
   if (code) {
     const supabase = await createClientForServer();
     const { error } = await supabase.auth.exchangeCodeForSession(code);

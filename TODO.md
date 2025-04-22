8ccc0885011ef9cd2ae9fe0ac41f1ce4e0f3b16c
Vignesh Chandrasekharan <vchan444@gmail.com>
2025-04-23 01:33:27 +0530

    vercel analytics

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 1. optimize the root page.tsx file to render more parts in the server-side
 2. remove auth logic for email/pw setup (not needed)
-3. payments processing
-4. remove dark mode for the landing page
-5. add email confirmation after payment
-6. add credits to account after payment
+3. remove dark mode for the landing page
+4. add email confirmation after payment
+5. add credits to account after payment
+6. logout button
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 import { ReactQueryClientProvider } from "@/components/ReactQueryClientProvider";
@@ -42,6 +43,7 @@ export default function RootLayout({
             {children}
             <Toaster duration={6000} theme="light" richColors />
             <SpeedInsights />
+            <Analytics />
           </ThemeProvider>
         </body>
       </html>

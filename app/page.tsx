43e27a64cde5643bdf7936cd9d2628d581d55694
Vignesh Chandrasekharan <vchan444@gmail.com>
2026-07-16 02:56:53 +0530

    comment test

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import { Footer } from "../components/landing/Footer";
 import { FAQ } from "@/components/landing/FAQ";
 import { FreeCreditBanner } from "@/components/landing/FreeCreditBanner";
 
+// test comment
 export default function Home() {
   return (
     <div className="min-h-screen bg-background bg-gradient-to-b from-violet-50 via-white to-blue-50">
